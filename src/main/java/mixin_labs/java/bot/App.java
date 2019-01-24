de8d7b8ececfcb8e48aaa00abf3b0366bb9c3a5a
wenewzhang <wenewboy@gmail.com>
2019-01-24 13:56:19 +0800

    hello,world

--- src/main/java/mixin_labs/java/bot/App.java
+++ src/main/java/mixin_labs/java/bot/App.java
@@ -11,7 +11,8 @@ import java.security.PrivateKey;
 import java.security.interfaces.RSAPrivateKey;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-
+// import java.util.Base64;
+import org.apache.commons.codec.binary.Base64;
 import okhttp3.Response;
 import okhttp3.WebSocket;
 import okhttp3.WebSocketListener;
@@ -46,54 +47,72 @@ public class App {
             System.out.println("json: " + msgIn);
             JsonObject obj = new JsonParser().parse(msgIn).getAsJsonObject();
             MIXIN_Action action = MIXIN_Action.parseFrom(obj);
+            System.out.println(action);
             MIXIN_Category category = MIXIN_Category.parseFrom(obj);
-            if (action == MIXIN_Action.CREATE_MESSAGE && category != null) {
+            System.out.println(category);
+            if (action == MIXIN_Action.CREATE_MESSAGE && obj.get("data") != null &&
+                category != null ) {
+              String messageId =
+                    obj.get("data").getAsJsonObject().get("message_id").getAsString();
+              MixinBot.sendMessageAck(webSocket, messageId);
               switch (category) {
                 case PLAIN_TEXT:
-                case PLAIN_STICKER:
-                case PLAIN_IMAGE:
-                case PLAIN_CONTACT:
-                  // 确认收到的消息，使得对方界面的消息状态，由单钩变双钩
-                  String messageId =
-                    obj.get("data").getAsJsonObject().get("message_id").getAsString();
-                  MixinBot.sendMessageAck(webSocket, messageId);
-
-                  // 回复给对方消息
-                  String conversationId =
-                    obj.get("data").getAsJsonObject().get("conversation_id").getAsString();
-                  String userId =
-                    obj.get("data").getAsJsonObject().get("user_id").getAsString();
-                  System.out.println("conversationId = " + conversationId);
-                  System.out.println("userId = " + userId);
-
-                  MixinBot.sendText(
-                    webSocket,
-                    conversationId,
-                    userId,
-                    "很高兴见到你！");
-
-                  MixinBot.sendSticker(
-                    webSocket,
-                    conversationId,
-                    userId,
-                    "eyJhbGJ1bV9pZCI6IjM2ZTk5NzdjLTJiYWItNDNjYS1hMmI2LTdlMDFmNWViNjhkZSIsIm5hbWUiOiJpbGx1c2lvbiJ9");
-
-                  MixinBot.sendContact(
-                    webSocket,
-                    conversationId,
-                    userId,
-                    "0c21b607-5e5b-461b-963f-95708346c21d");
-
-                  // MixinBot.transferTo(
-                  //   "965e5c6e-434c-3fa9-b780-c50f43cd955c", // CNB
-                  //   userId,
-                  //   1.0
-                  // );
-
-                  break;
-                default:
-                  System.out.println("Category: " + category);
+                    String conversationId =
+                      obj.get("data").getAsJsonObject().get("conversation_id").getAsString();
+                    String userId =
+                      obj.get("data").getAsJsonObject().get("user_id").getAsString();
+                    byte[] msgData = Base64.decodeBase64(obj.get("data").getAsJsonObject().get("data").getAsString());
+                    MixinBot.sendText(webSocket,conversationId,userId,new String(msgData,"UTF-8"));
+                    break;
+                  default:
+                    System.out.println("Category: " + category);
               }
+              // switch (category) {
+              //   case PLAIN_TEXT:
+              //   case PLAIN_STICKER:
+              //   case PLAIN_IMAGE:
+              //   case PLAIN_CONTACT:
+              //     // 确认收到的消息，使得对方界面的消息状态，由单钩变双钩
+              //     String messageId =
+              //       obj.get("data").getAsJsonObject().get("message_id").getAsString();
+              //     MixinBot.sendMessageAck(webSocket, messageId);
+              //
+              //     // 回复给对方消息
+              //     String conversationId =
+              //       obj.get("data").getAsJsonObject().get("conversation_id").getAsString();
+              //     String userId =
+              //       obj.get("data").getAsJsonObject().get("user_id").getAsString();
+              //     System.out.println("conversationId = " + conversationId);
+              //     System.out.println("userId = " + userId);
+              //
+              //     MixinBot.sendText(
+              //       webSocket,
+              //       conversationId,
+              //       userId,
+              //       "很高兴见到你！");
+              //
+              //     MixinBot.sendSticker(
+              //       webSocket,
+              //       conversationId,
+              //       userId,
+              //       "eyJhbGJ1bV9pZCI6IjM2ZTk5NzdjLTJiYWItNDNjYS1hMmI2LTdlMDFmNWViNjhkZSIsIm5hbWUiOiJpbGx1c2lvbiJ9");
+              //
+              //     MixinBot.sendContact(
+              //       webSocket,
+              //       conversationId,
+              //       userId,
+              //       "0c21b607-5e5b-461b-963f-95708346c21d");
+              //
+              //     // MixinBot.transferTo(
+              //     //   "965e5c6e-434c-3fa9-b780-c50f43cd955c", // CNB
+              //     //   userId,
+              //     //   1.0
+              //     // );
+              //
+              //     break;
+              //   default:
+              //     System.out.println("Category: " + category);
+              // }
             }
           } catch (Exception e) {
             e.printStackTrace();
